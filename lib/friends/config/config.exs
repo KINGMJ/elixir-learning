643bc6ed675d0bd71c3537bd057a435a4c35966c
jerry.mei <328047478@qq.com>
2020-03-26 14:03:38 +0800

    add ecto

---
+++ lib/friends/config/config.exs
@@ -0,0 +1,9 @@
+import Config
+
+config :friends, Friends.Repo,
+  database: "friends_repo",
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost"
+
+config :friends, ecto_repos: [Friends.Repo]
