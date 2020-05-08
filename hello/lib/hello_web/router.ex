7dfb5dfcb3322e22da1257d841c93709dad5dc78
jerry.mei <jerry.mei1022@qq.com>
2020-05-08 22:08:03 +0800

    guide 练习

--- hello/lib/hello_web/router.ex
+++ hello/lib/hello_web/router.ex
@@ -7,6 +7,7 @@ defmodule HelloWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug HelloWeb.Plugs.Locale, "en"
   end
 
   pipeline :api do
@@ -17,6 +18,15 @@ defmodule HelloWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/hello", HelloController, :index
+    get "/hello/:messenger", HelloController, :show
+    get "/", RootController, :index
+
+    resources "/users", UserController do
+        resources "/posts", PostController, only: [:index, :show]
+    end
+
+    resources "/comments", CommentController, except: [:delete]
   end
 
   # Other scopes may use custom stacks.
