aea1ea90dfcd74f92acd3331255555b7b882084f
jerry.mei <jerry.mei@jerrymeideMacBook-Pro.local>
2020-05-14 10:57:45 +0800

    add controller practice

--- hello/lib/hello_web/controllers/hello_controller.ex
+++ hello/lib/hello_web/controllers/hello_controller.ex
@@ -1,11 +1,33 @@
 defmodule HelloWeb.HelloController do
   use HelloWeb, :controller
+  import Plug.Conn
 
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
-  def show(conn, %{"messenger"=> messenger} = params) do
-    render(conn, "show.html", messenger: messenger)
+  def show(conn, %{"messenger" => messenger} = params) do
+    ## render(conn, "show.html", messenger: messenger)
+    ## text(conn, "From messenger #{messenger}")
+    ## json(conn, %{id: messenger})
+
+    html(conn, """
+      <html>
+        <head>
+          <title>Passing a Messenger</title>
+        </head>
+        <body>
+          <p>From messenger #{messenger}</p>
+        </body>
+      </html>
+    """)
   end
+
+  # def show(conn, params) do
+  #   conn
+  #   |> assign(:messenger, messenger)
+  #   |> assign(:receiver, receiver)
+  #   |> render("show.html")
+  # end
+
 end
