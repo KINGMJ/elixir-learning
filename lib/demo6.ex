ef35d8a367aaf033b2d63732f977097bb80b1548
KINGMJ <328047478@qq.com>
2021-09-28 16:21:46 +0800

    add new demo

--- lib/demo6.ex
+++ lib/demo6.ex
@@ -1,9 +1,15 @@
+defmodule Sayings.Greetings do
+  def basic(name), do: "Hi, #{name}"
+end
+
 defmodule Example do
   import Sayings.Greetings
 
   def print_message(name), do: basic(name)
 end
 
-defmodule Sayings.Greetings do
-  def basic(name), do: "Hi, #{name}"
+defmodule Example1 do
+  alias Sayings.Greetings, as: Hi
+
+  def print_message(name), do: Hi.basic(name)
 end
